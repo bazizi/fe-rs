bbfca156f0c54ac80b86a09ac719608f22249a08
Behnam Azizi <>
2024-05-18 14:40:49 -0700

    working history

---
+++ TODO.md
@@ -0,0 +1,12 @@
+- Feature: Parent directory + shortcut key
+- Feature: filtering
+- Bugfix: items outside the rendering Window
+- Bugfix: Hidden items in the list / cursor getting disabled for invisible items
+- Feature: Different icons for different file types
+- Feature: Non C:\ drives
+- Feature: columns for file size and last modified and file type
+- Feature: sort by columns
+- Feature: bookmarks
+- Feature: state serialization
+- Feature: ability to open files using default program
+- Feature: Encrypt/decrypt files
--- src/components/home.rs
+++ src/components/home.rs
@@ -1,128 +1,188 @@
-use std::{cmp::min, collections::HashMap, default, hash::Hash, path::Path, time::Duration, vec};
+use std::{
+    cmp::min,
+    collections::{HashMap, VecDeque},
+    default,
+    hash::Hash,
+    path::Path,
+    time::Duration,
+    vec,
+};
 
 use color_eyre::eyre::Result;
-use crossterm::event::{KeyCode, KeyEvent};
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use ratatui::{prelude::*, widgets::*};
 use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc::UnboundedSender;
 
 use super::{Component, Frame};
 use crate::{
-  action::Action,
-  config::{Config, KeyBindings},
+    action::Action,
+    config::{Config, KeyBindings},
 };
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 struct DirEntry {
-  path: String,
-  is_dir: bool,
-  size: Option<usize>,
+    path: String,
+    is_dir: bool,
+    size: Option<usize>,
+}
+
+#[derive(Default, Clone)]
+struct WorkingDirectory {
+    path: String,
+    children: Vec<Option<DirEntry>>,
 }
 
 #[derive(Default)]
 pub struct Home {
-  command_tx: Option<UnboundedSender<Action>>,
-  config: Config,
-  cwd: (String, Vec<Option<DirEntry>>),
-  curr_index: Option<usize>,
-  selected: bool,
+    command_tx: Option<UnboundedSender<Action>>,
+    config: Config,
+    cwd: Option<WorkingDirectory>,
+    history_backward: Vec<WorkingDirectory>,
+    history_forward: Vec<WorkingDirectory>,
+    curr_index: Option<usize>,
+    selected: bool,
 }
 
 impl Home {
-  pub fn new() -> Self {
-    Self { cwd: ("/".to_string(), vec![]), ..Self::default() }
-  }
+    pub fn new() -> Self {
+        Self { cwd: Some(WorkingDirectory { path: "\\".to_string(), children: vec![] }), ..Self::default() }
+    }
+}
+
+fn get_dir_entry_icon(dir_entry_text: &str) -> String {
+    if dir_entry_text.ends_with(".mp3") {
+        '🎹'.to_string()
+    } else if dir_entry_text.starts_with(".") {
+        '⚙'.to_string()
+    } else if dir_entry_text.ends_with(".exe") {
+        '💾'.to_string()
+    } else if dir_entry_text.ends_with(".zip") {
+        '🗜'.to_string()
+    } else {
+        '📃'.to_string()
+    }
 }
 
 impl Component for Home {
-  fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
-    self.command_tx = Some(tx);
-    Ok(())
-  }
+    fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
+        self.command_tx = Some(tx);
+        Ok(())
+    }
 
-  fn register_config_handler(&mut self, config: Config) -> Result<()> {
-    self.config = config;
-    Ok(())
-  }
+    fn register_config_handler(&mut self, config: Config) -> Result<()> {
+        self.config = config;
+        Ok(())
+    }
 
-  fn handle_key_events(&mut self, key: KeyEvent) -> Result<Option<Action>> {
-    match key.code {
-      KeyCode::Char('j') | KeyCode::Down => {
-        if self.curr_index.is_some() {
-          self.curr_index.replace(self.curr_index.unwrap() + 1);
+    fn handle_key_events(&mut self, key: KeyEvent) -> Result<Option<Action>> {
+        match key.code {
+            KeyCode::Char('j') | KeyCode::Down => {
+                if self.curr_index.is_some() {
+                    self.curr_index.replace(self.curr_index.unwrap() + 1);
+                }
+            },
+            KeyCode::Char('k') | KeyCode::Up => {
+                if self.curr_index.is_some() && Some(0) != self.curr_index {
+                    self.curr_index.replace(self.curr_index.unwrap() - 1);
+                }
+            },
+            KeyCode::Enter => {
+                self.selected = true;
+            },
+            KeyCode::Left => {
+                if key.modifiers & KeyModifiers::ALT == KeyModifiers::ALT {
+                    if let Some(history_item) = self.history_backward.pop() {
+                        self.history_forward.push(self.cwd.take().unwrap());
+                        self.cwd = Some(history_item);
+                    }
+                }
+            },
+            KeyCode::Right => {
+                if key.modifiers & KeyModifiers::ALT == KeyModifiers::ALT {
+                    if let Some(history_item) = self.history_forward.pop() {
+                        self.history_backward.push(self.cwd.take().unwrap());
+                        self.cwd = Some(history_item);
+                    }
+                }
+            },
+            _ => {},
         }
-      },
-      KeyCode::Char('k') | KeyCode::Up => {
-        if self.curr_index.is_some() && Some(0) != self.curr_index {
-          self.curr_index.replace(self.curr_index.unwrap() - 1);
+
+        Ok(None)
+    }
+
+    fn update(&mut self, action: Action) -> Result<Option<Action>> {
+        match action {
+            Action::Tick => {
+                // if self.selected {
+                //   self.cwd =
+                // }
+            },
+            Action::Help => {},
+            _ => {},
         }
-      },
-      KeyCode::Enter => self.selected = true,
-      _ => {},
+        Ok(None)
     }
 
-    Ok(None)
-  }
-
-  fn update(&mut self, action: Action) -> Result<Option<Action>> {
-    match action {
-      Action::Tick => {
-        // if self.selected {
-        //   self.cwd =
-        // }
-      },
-      Action::Help => {},
-      _ => {},
-    }
-    Ok(None)
-  }
-
-  fn draw(&mut self, f: &mut Frame<'_>, area: Rect) -> Result<()> {
-    if self.selected && self.curr_index.is_some() {
-      self.cwd = (self.cwd.1[self.curr_index.unwrap()].take().unwrap().path, vec![]);
-      self.selected = false;
-    }
-
-    if self.cwd.1.is_empty() {
-      self.cwd.1.push(Some(DirEntry { path: "..".to_owned(), ..DirEntry::default() }));
-      if let Ok(res) = Path::new(&self.cwd.0).read_dir() {
-        for entry in res.flatten() {
-          let path = entry.path().to_str().unwrap().to_string();
-          self.cwd.1.push(Some(DirEntry { is_dir: Path::new(&path).is_dir(), path, ..DirEntry::default() }));
+    fn draw(&mut self, f: &mut Frame<'_>, area: Rect) -> Result<()> {
+        if self.selected && self.curr_index.is_some() {
+            let selected_item =
+                self.cwd.as_ref().unwrap().children[*self.curr_index.as_ref().unwrap()].as_ref().unwrap().path.clone();
+            if Path::new(&selected_item).is_dir() {
+                let cwd = self.cwd.take().unwrap();
+                self.cwd = Some(WorkingDirectory { path: selected_item, children: vec![] });
+                self.history_backward.push(cwd);
+            }
+            self.selected = false;
         }
-      }
+
+        let cwd = self.cwd.as_mut().unwrap();
+
+        if cwd.children.is_empty() {
+            cwd.children.push(Some(DirEntry { path: "..".to_owned(), ..DirEntry::default() }));
+            if let Ok(res) = Path::new(&cwd.path).read_dir() {
+                for entry in res.flatten() {
+                    let path = entry.path().to_str().unwrap().to_string();
+                    cwd.children.push(Some(DirEntry {
+                        is_dir: Path::new(&path).is_dir(),
+                        path,
+                        ..DirEntry::default()
+                    }));
+                }
+            }
+        }
+
+        if self.curr_index.is_none() {
+            if !cwd.children.is_empty() {
+                self.curr_index = Some(0);
+            }
+        } else {
+            self.curr_index = Some(min(*self.curr_index.as_ref().unwrap(), cwd.children.len() - 1));
+        }
+
+        let lines = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(cwd.children.iter().map(|_| Constraint::Length(1)).collect::<Vec<Constraint>>())
+            .split(area);
+
+        for i in 0..cwd.children.len() {
+            let dir_entry = cwd.children[i].as_ref().unwrap();
+            let mut dir_entry_text = dir_entry.path.clone();
+            if dir_entry.is_dir {
+                dir_entry_text = "📂".to_owned() + &dir_entry_text;
+            } else {
+                dir_entry_text = get_dir_entry_icon(&dir_entry_text) + &dir_entry_text;
+            }
+
+            let mut paragraph = Paragraph::new(dir_entry_text.as_str());
+            let is_selected = self.curr_index.is_some() && Some(i) == self.curr_index;
+            if is_selected {
+                paragraph = paragraph.set_style(Style::new().bg(Color::Magenta));
+            }
+            f.render_widget(paragraph, lines[i]);
+        }
+
+        Ok(())
     }
-
-    if self.curr_index.is_none() {
-      if !self.cwd.1.is_empty() {
-        self.curr_index = Some(0);
-      }
-    } else {
-      self.curr_index = Some(min(*self.curr_index.as_ref().unwrap(), self.cwd.1.len() - 1));
-    }
-
-    let lines = Layout::default()
-      .direction(Direction::Vertical)
-      .constraints(self.cwd.1.iter().map(|_| Constraint::Length(1)).collect::<Vec<Constraint>>())
-      .split(area);
-
-    for i in 0..self.cwd.1.len() {
-      let dir_entry = self.cwd.1[i].as_ref().unwrap();
-      let mut dir_entry_text = dir_entry.path.clone();
-      if dir_entry.is_dir {
-        dir_entry_text = "📂".to_owned() + &dir_entry_text;
-      } else {
-        dir_entry_text = "📄".to_owned() + &dir_entry_text;
-      }
-
-      let mut paragraph = Paragraph::new(dir_entry_text.as_str());
-      let is_selected = self.curr_index.is_some() && Some(i) == self.curr_index;
-      if is_selected {
-        paragraph = paragraph.set_style(Style::new().bg(Color::Magenta));
-      }
-      f.render_widget(paragraph, lines[i]);
-    }
-
-    Ok(())
-  }
 }
